4e8763beeab9e83fa338f9eef4cebe8a15be2be7
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-03-24 00:41:43 +0430

    add put and patch for data

--- app/Http/Requests/V1/StoreCustomerRequest.php
+++ app/Http/Requests/V1/StoreCustomerRequest.php
@@ -34,8 +34,10 @@ class StoreCustomerRequest extends FormRequest
     }
 
     protected function prepareForValidation() {
-        $this->merge([
-            'postal_code' => $this->postalCode
-        ]);
+        if($this->postalCode) {
+            $this->merge([
+                'postal_code' => $this->postalCode
+            ]);
+        }
     }
 }
